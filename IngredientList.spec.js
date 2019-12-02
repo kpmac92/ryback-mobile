cecbb8b59b57df98b60b4c7f60cab3ef0c73b21b
Kevin Patrick <kpmac92@gmail.com>
2019-12-01 20:03:55 -0500

    Add mocha and chai; test runs but unable to import component

---
+++ IngredientList.spec.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import IngredientList from './IngredientList';
+
+describe('Ingredient List', () => {
+    it('should pass', () => {
+        // const subject = <IngredientList ingredients={[]}/>;
+        console.log('Success!');
+    })
+
+})
