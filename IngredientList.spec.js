d2b521e370297b4648d64722062630d03d311c5c
Kevin Patrick <kpmac92@gmail.com>
2019-12-07 14:21:21 -0500

    create basic test for ingredient list

--- IngredientList.spec.js
+++ IngredientList.spec.js
@@ -1,10 +1,12 @@
 import { expect } from 'chai';
+import React from 'react';
+import { render } from '@testing-library/react-native';
 import IngredientList from './IngredientList';
 
 describe('Ingredient List', () => {
     it('should pass', () => {
-        // const subject = <IngredientList ingredients={[]}/>;
-        console.log('Success!');
+        const { getAllByText, baseElement } = render(<IngredientList ingredients={[]}/>);
+        expect(getAllByText('Ingredients')).to.have.length(1);
     })
 
 })
